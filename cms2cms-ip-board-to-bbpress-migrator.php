b43b574ca868594743b8e3d097c3a7ab62147143
cms2cms <cms2cms@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-14 12:24:00 +0000

    Update the Connection Bridge
    git-svn-id: https://plugins.svn.wordpress.org/cms2cms-ip-board-to-bbpress-migrator/tags/3.6.2@913836 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cms2cms-ip-board-to-bbpress-migrator.php
+++ cms2cms-ip-board-to-bbpress-migrator.php
@@ -1,8 +1,8 @@
 <?php
 /*
-    Plugin Name: CMS2CMS IP Board to WordPress migration
+    Plugin Name: CMS2CMS IPBoard to WordPress migration
     Plugin URI: http://www.cms2cms.com
-    Description: Migrate your website content from IP Board to WordPress easily and automatedly in just a few simple steps.
+    Description: Migrate your website content from IPBoard to WordPress easily and automatedly in just a few simple steps.
     Version: 1.0.1
     Author: MagneticOne
     Author URI: http://magneticone.com
